07121725ba1ad1621a4dbb279bdc05fe63e4fdd8
sjy <deanyoyn99@wattsolution.co.kr>
2023-07-04 16:33:11 +0900

    feautre-ysj: env 설정 작업

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,10 @@ export default {
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: ["@nuxtjs/eslint-module"],
+  buildModules: [
+    "@nuxtjs/eslint-module",
+    ["@nuxtjs/dotenv", { path: "./" }],
+  ],
 
   /*
    ** Nuxt.js modules
@@ -130,27 +133,27 @@ export default {
     port: 8205
   },
   publicRuntimeConfig: {
-    backendURL_local:process.env.backendURL_local,
-    backendURL:process.env.backendURL,
-    powertlakLogin_local:process.env.powertlakLogin_local,
-    powertalkLogin:process.env.powertalkLogin,
-    powermemo:process.env.powermemo,
-    powermemoSavefolder:process.env.powermemoSavefolder,
-    qr_filePath:process.env.qr_filePath,
-    qr:process.env.qr,
-    att_access_user:process.env.att_access_user,
-    att_videoPlayPath:process.env.att_videoPlayPath,
-    att_filePath:process.env.att_filePath,
-    detailFilePath:process.env.detailFilePath,
-    fileUploadBackend:process.env.fileUploadBackend,
-    useEnterprise:process.env.useEnterprise,
-    use_bento4:process.env.use_bento4,
-    elecQR:process.env.elecQR,
-    elecFilePath:process.env.elecFilePath,
-    elecFolderPath:process.env.elecFolderPath,
-    fileBoxBackend:process.env.fileBoxBackend,
-    separateBackendUploadPath:process.env.separateBackendUploadPath,
-    impId:process.env.impId,
-    pg:process.env.pg
+    backendURL_local: process.env.backendURL_local,
+    backendURL: process.env.backendURL,
+    powertlakLogin_local: process.env.powertlakLogin_local,
+    powertalkLogin: process.env.powertalkLogin,
+    powermemo: process.env.powermemo,
+    powermemoSavefolder: process.env.powermemoSavefolder,
+    qr_filePath: process.env.qr_filePath,
+    qr: process.env.qr,
+    att_access_user: process.env.att_access_user,
+    att_videoPlayPath: process.env.att_videoPlayPath,
+    att_filePath: process.env.att_filePath,
+    detailFilePath: process.env.detailFilePath,
+    fileUploadBackend: process.env.fileUploadBackend,
+    useEnterprise: process.env.useEnterprise,
+    use_bento4: process.env.use_bento4,
+    elecQR: process.env.elecQR,
+    elecFilePath: process.env.elecFilePath,
+    elecFolderPath: process.env.elecFolderPath,
+    fileBoxBackend: process.env.fileBoxBackend,
+    separateBackendUploadPath: process.env.separateBackendUploadPath,
+    impId: process.env.impId,
+    pg: process.env.pg
   }
 }
