ae63c1d6caf3d68c84f10f1d8bfe6b5ff16261fc
sjy <deanyoyn99@wattsolution.co.kr>
2023-09-12 11:01:33 +0900

    feature-ysj: logsheet 고도화

--- client/pages/logSheet/index.vue
+++ client/pages/logSheet/index.vue
@@ -42,44 +42,67 @@
         methods: {
             childData(params) {
                 const url = params.current_path
+                if (url) {
+                    this.childPath(url)
+                }
+            },
+            childPath(url) {
                 if (url != "/wattmanager2/safetycheck") {
                     const originURL = process.env.logsheetURL.split("/")
                     this.refreshURL  = originURL[0] + "//" + originURL[2] + url
+                    sessionStorage.setItem("refreshURL", this.refreshURL)
                 } else {
                     this.refreshURL =
-                    process.env.logsheetURL +
-                    "?en_seq=" + localStorage.getItem("enSeq") +
-                    "&hq_seq=" + localStorage.getItem("hqSeq") +
-                    "&br_seq="  + localStorage.getItem("brSeq") +
-                    "&version=1"
+                        process.env.logsheetURL +
+                        "?en_seq=" + localStorage.getItem("enSeq") +
+                        "&hq_seq=" + localStorage.getItem("hqSeq") +
+                        "&br_seq="  + localStorage.getItem("brSeq") +
+                        "&version=1"
+                    sessionStorage.setItem("refreshURL", this.refreshURL)
                 }
-                sessionStorage.setItem("refreshURL", this.refreshURL)
+                this.logsheetURL = this.refreshURL
+                this.calcHeight()
+            },
+            calcHeight() {
                 if (this.logsheetInterval) {
                     clearInterval(this.logsheetInterval)
                 }
                 // iframe 높이 초기화 먼저 진행
                 document.getElementById("logSheet").style.height = "auto"
                 document.getElementsByClassName("mainFooter")[0].style.display = "none"
+                // wattmanager2의 data가 많은 경우 data를 전부 가져 오기 전에 높이를 구해 버린다. 이를 방지하기 위해 계속 체크해준다
                 this.logsheetInterval = setInterval(() => {
-                    const test = document.getElementById("logSheet").contentWindow.document.getElementById("root")
-                    const removeDiv = document.getElementById("logSheet").contentWindow.document.getElementsByClassName("sc-dIfARi eptRVp")[0]
-                    removeDiv.style.height = "0%"
-                    this.wattmanger2Height = test.offsetHeight + "px"
+                    const wattmanager2MainWrap = document.getElementById("logSheet").contentWindow.document.getElementById("root")
+                    const wattmanager2BottomDiv = document.getElementById("logSheet").contentWindow.document.getElementsByClassName("sc-dIfARi eptRVp")[0]
+                    // wattmanager2BottomDiv height 10vh를 덮어 씌움
+                    wattmanager2BottomDiv.style.height = "40px"
+                    // wattmanager2에서 min-height 10vh로 지정한거를 덮어 씌움
+                    wattmanager2BottomDiv.style.minHeight = "40px"
+                    this.wattmanger2Height = wattmanager2MainWrap.offsetHeight + "px"
                     document.getElementById("logSheet").style.height = this.wattmanger2Height
                     document.getElementsByClassName("mainFooter")[0].style.display = "flex"
                     document.getElementsByClassName("mainFooter")[0].style.bottom = "0px"
                 }, 400)
                 setTimeout(() => {
                     clearInterval(this.logsheetInterval)
-                }, 10000)
+                }, 20000)
+            },
+            scrollInToTop() {
+                document.getElementById("logSheet").scrollIntoView({behavior: "smooth"})
             }
         },
+        beforeDestroy() {
+            if (this.logsheetInterval) {
+                clearInterval(this.logsheetInterval)
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
 .mainWrap {
     width: 100%;
+    background-color: #f7f7f7;
 }
 iframe {
     width: 100%;
