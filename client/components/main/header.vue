abe23a244122c0597f8da116118fe7581de3b739
sjy <deanyoyn99@wattsolution.co.kr>
2024-01-08 16:15:13 +0900

    feature-ysj: send logsheet tab title

--- client/components/main/header.vue
+++ client/components/main/header.vue
@@ -17,9 +17,9 @@
         <a v-if="authority == '4' && qrStatus == 'power'" href="/qr" class="col-auto">{{ $t("powerQR") }}</a>
         <a v-if="authority == '4'" href="/integrationQr">{{ $t("printQR")[0] }}</a>
         <a v-if="authority == '4'" href="/upload?page=1&viewType=upload" class="col-auto">{{ $t("upload")}}</a>
-        <!-- <a v-if="showTBM || authority == '4'" class="col-auto" id="logsheetBtn" @click="hrefSmartTbm()">{{ $t("smartTBM") }}</a> -->
-        <a v-if="logSheet == 'true' && location !== 'kepco.watttalk.kr'" id="logsheetBtn" class="col-auto" @click="hrefLogsheet()">{{ $t("logSheet") }}</a>
-        <a v-if="logSheet == 'true' && location == 'kepco.watttalk.kr'" id="logsheetBtn" class="col-auto" @click="hrefLogsheet()">{{  $t("kepcologSheet") }}</a>
+        <!-- <a v-if="showTBM || authority == '4'" class="col-auto" id="tbmBtn" @click="hrefSmartTbm()">{{ $t("smartTBM") }}</a> -->
+        <a v-if="logSheet == 'true' && location !== 'kepco.watttalk.kr'" id="logsheetBtn" name="logsheetTitle" class="col-auto" @click="hrefLogsheet()">{{ $t("logSheet") }}</a>
+        <a v-if="logSheet == 'true' && location == 'kepco.watttalk.kr'" id="logsheetBtn" name="logsheetTitle" class="col-auto" @click="hrefLogsheet()">{{  $t("kepcologSheet") }}</a>
         <a class="col-auto" href="/upload?page=1&viewType=filebox">{{ $t("fileBox") }}</a>
         <a class="col-auto" href="/notice?page=1">{{ $t("notice")[0] }}</a>
         <a class="col-auto" href="/profile">{{ $t("profile") }}</a>
