080706f9acbbac8390f1dc6e9d7361d19bb455fc
sjy <deanyoyn99@wattsolution.co.kr>
2024-02-19 07:54:22 +0900

    feature-ysj: kepco tbm title fix

--- client/pages/logSheet/index.vue
+++ client/pages/logSheet/index.vue
@@ -44,7 +44,7 @@
                         "&br_seq=" + sessionStorage.getItem("brSeq") +
                         "&auth=" + sessionStorage.getItem("auth") +
                         "&version=1&lang=" + sessionStorage.getItem("languageCode") +
-                        "&logsheetTitle='TBM관리'" +
+                        "&logsheetTitle=TBM관리" +
                         "&template_id=comunicationtbm"
                 } else {
                     // test를 위해 "&template_id=comunicationtbm" 붙어있음. 추후 제거
@@ -55,7 +55,7 @@
                         "&br_seq=" + sessionStorage.getItem("brSeq") +
                         "&auth=" + sessionStorage.getItem("auth") +
                         "&version=1&lang=" + sessionStorage.getItem("languageCode") +
-                        "&logsheetTitle='로그시트'" +
+                        "&logsheetTitle=로그시트" +
                         "&template_id=''"
                 }
             } // 로그시트 첫페이지 새로고침 시
