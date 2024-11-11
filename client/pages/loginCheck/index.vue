19cbf482f0285b5cb53b21251e0cee212763049c
곽선영 <tjsdud1116@wattsolution.co.kr>
2024-11-11 17:52:39 +0900

    feat: 와트톡 마이페이지 라우팅 처리

--- client/pages/loginCheck/index.vue
+++ client/pages/loginCheck/index.vue
@@ -68,9 +68,12 @@ export default {
                         const cookieName = sessionStorage.getItem("id") + "ManagerLoginTime"
                         cookieSetting.setCookie(cookieName, currentTime)
                     }
+                    const linkType = this.$route.query.type
                     // watt talk에서 여는 경우 로그인 정보 확인 후 attchment로 보내줌
-                    if (this.$route.query.type == "video") {
+                    if (linkType == "video") {
                         window.location.href = "attachment/video?page=1&viewType=gallery&lang=" + this.$route.query.lang
+                    } else if (linkType == "mypage") {
+                        window.location.href = "profile?lang=" + this.$route.query.lang
                     } else if (this.$route.query.type == "profile") {
                         // 비밀번호 변경 안내 모달을 통해 접근 한 경우 내정보 > 비밀번호 변경을 실행시켜준다
                         window.open("/profile?changePsw=true", "_self")
